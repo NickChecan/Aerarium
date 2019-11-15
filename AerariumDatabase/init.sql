aedeee8d4060127b495be068d8a838d29e05d097
Nicholas <nicholas.checan@gmail.com>
2019-11-15 01:05:43 -0300

    Fixing database integration

--- AerariumDatabase/init.sql
+++ AerariumDatabase/init.sql
@@ -1,5 +1,4 @@
 -- Create a Aerarium database instance if not exists
-CREATE DATABASE aerarium;
 SELECT 'CREATE DATABASE aerarium'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'aerarium')\gexec
 
